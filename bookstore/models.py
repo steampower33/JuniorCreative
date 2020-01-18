515a18596dc52e56a0a16f9037737a740ad9386a
seungbok3240 <seungbok3240@gmail.com>
2020-01-19 03:07:15 +0900

    Recommand book algorithm
    Lee seung bok

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+
+    class TYPE_OF_BOOK(models.TextChoices):
+        COMPUTER = 'COM', _('Computer')
+        NATURAL_SCIENCE = 'NAT', _('Natual_Science')
+        ECONOMY = 'ECO', _('ECONOMY')
+        HISTORY = 'HIS', _('History')
+        
+    class TYPE(models.TextChoices):
+        GAME = 'GAM', _('Game')
+
+    type_of_book = models.CharField(
+        max_length=10,
+        choices=TYPE_OF_BOOK.choices,
+        default=True,
+    )
+
+    type2 = models.CharField(
+        max_length=10,
+        choices=TYPE.choices,
+        default=True
+    )
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    published_date = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    author = models.CharField(max_length=50)
+    text = models.TextField()
+    grade = models.IntegerField(default=1)
+    created_date = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.text
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
+from django.db.models import Avg
+from .models import Book
 
 # Create your views here.
 def homepage(request):
     return render(request, 'bookstore/homepage.html')
 
+# 컴퓨터
 def computer(request):
-    return render(request, 'bookstore/computer.html')
+    return render(request, 'bookstore/computer/computer.html')
+
+def game(request):
+    books = Book.objects.filter(type_of_book='COM').values('title', 'reviews__text', 'reviews__author', 'reviews__grade').annotate(Avg('reviews__grade')).order_by('-reviews__grade__avg')
+    book_for_title = books[0]
+    final_books = books.filter(title=book_for_title['title'])
+    return render(request, 'bookstore/computer/game.html', {
+        'books' : books,
+        'book_for_title' : book_for_title,
+        'final_books' : final_books
+    })
+
+def graphic(request):
+    return render(request, 'bookstore/computer/graphic.html')
 
 def network(request):
-    return render(request, 'bookstore/network.html')
+    return render(request, 'bookstore/computer/network.html')
+
+def mobile(request):
+    return render(request, 'bookstore/computer/mobile.html')
+
+# 자연과학
+
+
+# 경제 경영
+
+# 역사
