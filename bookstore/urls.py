c47070126b3ed141fa77151f01558745fa65ac72
seungbok3240 <seungbok3240@gmail.com>
2020-01-19 02:46:55 +0900

    Recommand book algorithm
    Lee seung bok

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     #path('login/', auth_views.LoginView.as_view(), name = 'login'),
     #path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('computer/', views.computer, name = 'computer'),
-    path('computer/game/', views.game, name='game'),
-    path('computer/graphic/', views.graphic, name='graphic'),
     path('computer/network/', views.network, name = 'network'),
-    path('computer/mobile/', views.mobile, name='mobile'),
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -4,25 +4,8 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'bookstore/homepage.html')
 
-# 컴퓨터
 def computer(request):
-    return render(request, 'bookstore/computer/computer.html')
-
-def game(request):
-    return render(request, 'bookstore/computer/game.html')
-
-def graphic(request):
-    return render(request, 'bookstore/computer/graphic.html')
+    return render(request, 'bookstore/computer.html')
 
 def network(request):
-    return render(request, 'bookstore/computer/network.html')
-
-def mobile(request):
-    return render(request, 'bookstore/computer/mobile.html')
-
-# 자연과학
-
-
-# 경제 경영
-
-# 역사
+    return render(request, 'bookstore/network.html')
